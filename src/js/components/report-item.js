210add0814ffd4e98cb1cdc186047bc5555289ff
Victor Escalona <VictorEscalona@Victors-MacBook-Pro.local>
2015-11-17 21:39:19 +0000

    add thumbnails to cards

--- src/js/components/report-item.js
+++ src/js/components/report-item.js
@@ -8,14 +8,18 @@ class ReportItem extends React.Component {
         var _this = this;
         this.state = {
             styles: {
-                backgroundImage: 'url(' + _this.props.data.image + ')'
+                backgroundImage: 'url(' + _this.props.data.image + ')',
+                backgroundSize: '100%',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'bottom',
+                height: '120px'
             }
         }
     }
 
     render() {
         return (
-            <div className="card blue-grey darken-1" style={this.state.styles}>
+            <div className="card white darken-1" style={this.state.styles}>
                 <div className="card-content white-text">
                     <a className="card-title-holder">
                         <span className="card-title">{this.props.data.title}</span>
