1d95beca95d6eecafee807563be7c7625182ec20
Ryan Edwards <redwards@rapid7.com>
2015-11-17 22:07:26 +0000

    Added overlay

--- src/js/components/report-item.js
+++ src/js/components/report-item.js
@@ -23,17 +23,26 @@ class ReportItem extends React.Component {
     render() {
         if (this.props.data.editable) {
             return (
-
-                <div id="selectedCard">
-                    <input placeholder="Enter title"> {this.props.title} </input>
-                    <input placeholder="Enter url"> {this.props.title} </input>
-                    <button type="button" onClick={this.saveCard}> "Save" </button>
-                    <button type="button" onClick={this.clearFields}> "Cancel" </button>
+                <div className="card blue-grey darken-1 " data-type={this.props.data.type} style={this.state.styles}>
+                    <div id="selectedCard">
+                        <div className='card-content'>
+                            <input placeholder="Enter title"> {this.props.title} </input>
+                            <input placeholder="Enter url"> {this.props.title} </input>
+                            <button type="button" onClick={this.saveCard}> "Save" </button>
+                            <button type="button" onClick={this.clearFields}> "Cancel" </button>
+                        </div>
+                    </div>
                 </div>
             );
         } else {
             return (
-                <div className="card blue-grey darken-1 " data-type={this.props.data.type} style={this.state.styles}></div>
+                <div className="card blue-grey darken-1 " data-type={this.props.data.type} style={this.state.styles}>
+                    <div className="card-content white-text">
+                        <a className="card-title-holder">
+                            <span className="card-title">{this.props.data.title}</span>
+                        </a>
+                    </div>
+                </div>
             )
         }
 
